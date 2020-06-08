d6ea2322dde9ca5cf7b0afd59722d5660f075547
Danny Ohana <danny.ohana@gmail.com>
2020-06-08 12:42:21 -0400

    handle initial data from within App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +16,5 @@ class App extends Component {
   }
 }
 
-export default App
+// we don't need anything from the state so we leave the first parentheses invocation blank
+export default connect()(App);
