72cd266656f0ac8d317377dfcdc0ce3d9a4b63ad
Danny Ohana <danny.ohana@gmail.com>
2020-06-08 14:10:33 -0400

    dashboard component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
+import Dashboard from '../components/Dashboard';
 
 class App extends Component {
   componentDidMount() {
@@ -10,11 +11,21 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        {this.props.loading === true
+          ? null
+          : <Dashboard />}
       </div>
     )
   }
 }
 
-// we don't need anything from the state so we leave the first parentheses invocation blank
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+// We don't need anything from the state so we leave the first parentheses invocation blank.
+// Using the connect() function upgrades a component to a container.
+// Containers can read state from store and dispatch actions.
 export default connect()(App);
