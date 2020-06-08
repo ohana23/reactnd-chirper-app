46113a533f61323a9198071fcf6530a8c274382a
Danny Ohana <danny.ohana@gmail.com>
2020-06-08 14:52:56 -0400

    tweet ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
-import Dashboard from '../components/Dashboard';
+import Dashboard from './Dashboard';
 
 class App extends Component {
   componentDidMount() {
@@ -25,7 +25,6 @@ function mapStateToProps({ authedUser }) {
   }
 }
 
-// We don't need anything from the state so we leave the first parentheses invocation blank.
 // Using the connect() function upgrades a component to a container.
 // Containers can read state from store and dispatch actions.
-export default connect()(App);
+export default connect(mapStateToProps)(App);
